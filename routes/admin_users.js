3bb37dd5274804e42c20724f1ee0dc1ef4775809
megancarver06 <megan.carver@wsu.edu>
2024-04-18 16:52:51 -0700

    configured admin user and admin listing pages to display the users and listings in the database

--- routes/admin_users.js
+++ routes/admin_users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const Buyer = require('../models/Buyer');
+const Seller = require('../models/Seller');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('admin_users');
+router.get('/', async function(req, res, next) {
+  const buyers = await Buyer.findAll();
+  const sellers = await Seller.findAll();
+  res.render('admin_users', {buyers, sellers});
 });
 
 module.exports = router;
