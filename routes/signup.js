52ea3cd8bcfb243377e89348cf0f6c729c724bd4
AlbertLuca <albertlucas0719@outlook.com>
2024-04-18 17:03:40 -0700

    added switches for account type registration

--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const sessionChecker = (req, res, next) => {
+  if (req.session.user) {
+    res.redirect('/profile');
+  } else {
+    next();
+  }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('signup');
