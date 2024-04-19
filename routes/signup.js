84e7ecf4f9c79907ccdea7993529163c0137c591
AlbertLuca <albertlucas0719@outlook.com>
2024-04-19 15:00:23 -0700

    fixed shop encryption

--- routes/signup.js
+++ routes/signup.js
@@ -4,33 +4,36 @@ const User = require('../models/User');
 var crypto = require('crypto');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('signup');
 });
 
-router.post('/', async function(req, res, next) {
-  try {
-    const { username, password, usertype, email, shopname, shopdesc } = req.body;
-    
-    // Generate salt
-    let salt = crypto.randomBytes(16);
-    
-    // Derive key asynchronously
-    let derivedKey = await new Promise((resolve, reject) => {
-      crypto.pbkdf2(password, salt, 100000, 64, 'sha512', (err, key) => {
-        if (err) reject(err);
-        resolve(key.toString('hex'));
-      });
-    });
+router.post('/', async function (req, res, next) {
+  // Generate salt
+  let salt = crypto.randomBytes(16);
 
-    // Create user with derivedKey
-    const user = await User.create({ username, password: derivedKey, usertype, email, shopname, shopdesc });
-    res.redirect("/signin?msg=created");
+  // Derive key asynchronously
+  crypto.pbkdf2(req.body.password, salt, 100000, 64, 'sha512', async (err, hashedPassword) => {
+    if (err) {
+      console.error(err);
+      res.redirect("/signup?failed=1");
+    }
 
-  } catch(error) {
-    console.error(error);
-    res.redirect("/signup?failed=2");
-  }
+    try {
+      const { username, password, usertype, email, shopname, shopdesc } = req.body;
+
+
+      // Create user with derivedKey
+      const user = await User.create({ username, password: hashedPassword, usertype, email, shopname, shopdesc });
+      res.redirect("/signin?msg=created");
+
+    } catch (error) {
+      console.error(error);
+      res.redirect("/signup?failed=2");
+    }
+
+  });
+
 });
 
 module.exports = router;
