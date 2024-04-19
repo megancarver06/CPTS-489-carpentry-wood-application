44775b93bc450b00eca5ea13a3ba0627c532dede
megancarver06 <megan.carver@wsu.edu>
2024-04-18 21:50:10 -0700

    refactored Seller, Buyer, and Admin models into one User model with three type options

---
+++ models/User.js
@@ -0,0 +1,63 @@
+const { STRING } = require('mysql/lib/protocol/constants/types')
+const sequelize = require('../db')
+const { Model, DataTypes } = require('sequelize')
+
+class User extends Model {
+    static async findUser(username, password) {
+        try {
+            const user = await User.findByPk(username)
+            if(user && user.password === password) {
+                return user
+            } else {
+                return null
+            }
+        } catch(error) {
+            console.log(error)
+            return null
+        }
+    }
+
+    static async getUser(username) {
+        try {
+            const user = await User.findByPk(username)
+            return user ? user : null;
+        } catch(error) {
+            console.log(error)
+            return null
+        }
+    }
+}
+
+User.init({
+  // Model attributes are defined here
+  username: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  usertype: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  shopname : {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  shopdesc : {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+}, {
+  sequelize,
+  modelName: 'User'
+});
+
+module.exports = User
