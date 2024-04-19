d16699b14e021e106480c82437b8d82a2869abd6
AlbertLuca <albertlucas0719@outlook.com>
2024-04-18 19:13:38 -0700

    added Account Type option to sign up page

--- routes/signup.js
+++ routes/signup.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res, next) {
   res.render('signup');
 });
 
+router.post('/', async function(req, res, next) {
+  const { username, password, email, type } = req.body;
+  if(type === 'buyer') {
+    const Buyer = require('../models/Buyer');
+    const buyer = await Buyer.create({ username, password, email });
+    if(buyer) {
+      res.redirect('/signin');
+    } else {
+      res.redirect('/signup');
+    }
+  } else if(type === 'seller') {
+    const Seller = require('../models/Seller');
+    const seller = await Seller.create({ username, password, email });
+    if(seller) {
+      res.redirect('/signin');
+    } else {
+      res.redirect('/signup');
+    }
+  } else if(type === 'admin') {
+    const Admin = require('../models/Admin');
+    const admin = await Admin.create({ username, password, email });
+    if(admin) {
+      res.redirect('/signin');
+    } else {
+      res.redirect('/signup');
+    }
+  } else {
+    res.redirect('/signup');
+  }
+});
+
 module.exports = router;
