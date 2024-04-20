dbbf24ee8ad58edec214eef15021cf820df9079c
megancarver06 <megan.carver@wsu.edu>
2024-04-20 09:46:12 -0700

    added functionality to contact us form. Added new page that users are redirected to on successful form submission.

--- routes/contact_us.js
+++ routes/contact_us.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('contact_us');
 });
 
+router.post('/submit', function(req, res, next) {
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  const subject = req.body.subject;
+  const inquiry = req.body.inquiry;
+
+  if (validateEmail(email) == false) {
+    res.redirect("/contact_us/?msg=invalidemail");
+  }
+
+  res.render('contact_us_submitted');
+});
+
+function validateEmail(email) {
+  const valid = email.includes("@")
+  return valid;
+}
+
 module.exports = router;
