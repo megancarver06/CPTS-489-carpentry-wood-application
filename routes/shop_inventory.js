a1dd8d58e6227f6744d4ac762e133b7bf8b0123f
Briana Weik <briana.weik@wsu.edu>
2024-04-21 10:25:22 -0700

    Fixed broken links on the shop page; corrected typo in shop_inventory route

--- routes/shop_inventory.js
+++ routes/shop_inventory.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop_inventoty');
+  res.render('shop_inventory');
 });
 
 module.exports = router;
