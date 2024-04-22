bb814524e75cc4e579a56701da0690fb079afdfc
Briana Weik <briana.weik@wsu.edu>
2024-04-21 22:16:25 -0700

    Updated shop orders to be able to print invoices

--- routes/shop_orders.js
+++ routes/shop_orders.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/User');
 const Orders = require('../models/Orders');
+const Listing = require('../models/Listing');
 
 /* GET orders page for the shop. */
 router.get('/', async function(req, res, next) {
@@ -15,4 +16,11 @@ router.get('/', async function(req, res, next) {
   res.render('shop_orders', { orders, user });
 });
 
+router.post("/print_invoice/:id", async function(req, res, next) {
+  const order = await Orders.findOrders(req.params.id)
+  const listing = await Listing.findListing(order.listingid)
+  
+  res.render('invoice', {order, listing});
+
+});
 module.exports = router;
