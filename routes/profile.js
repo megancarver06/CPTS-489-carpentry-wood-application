5a6b39e1ae90990115a9d50353e9548ec70dbf52
AlbertLuca <albertlucas0719@outlook.com>
2024-04-21 21:54:15 -0700

    Added shop inventory to seller page

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const Listing = require('../models/Listing');
+const User = require('../models/User');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const user = req.user;
-  res.render('profile', { user: user });
+router.get('/', async function(req, res, next) {
+  const user = await User.getUser(req.user.username)
+  let shopname = user.shopname;
+  const listings = await Listing.findAll({
+    where: {
+      listingseller: shopname
+    }
+  });
+  res.render('profile', { listings, user });
 });
 
 module.exports = router;
